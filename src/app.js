8fdfa40c9fcdaf8fa912ed82fea74a7d8ea132e6
NatalyPetrova <zimnata96@mail.ru>
2023-12-12 20:10:41 +0300

    first

---
+++ src/app.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+const Div = styled.div`gir
+	text-align: center;
+`;
+
+export const App = () => {
+	return (
+		<Div>
+			<i className="fa fa-camera-retro"></i>
+			<div>123</div>
+		</Div>
+	);
+};
